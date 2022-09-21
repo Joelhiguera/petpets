a17a49e856e9d440509843c1e7aca76a8cc49ec8
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-20 21:07:19 -0600

    create navbar signup and login link

---
+++ petpets/src/components/Nav/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <ul className="flex-row">
+      <li className="mx-1">
+        <Link to="/signup">
+          Signup
+        </Link>
+      </li>
+      <li className="mx-1">
+        <Link to="/login">
+          Login
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default Nav;
