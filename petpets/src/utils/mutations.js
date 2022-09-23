78450e3b2f68064a60e3738060e22cf8e506b3a1
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-22 19:11:50 -0600

    create add order mutation in mutations.js

--- petpets/src/utils/mutations.js
+++ petpets/src/utils/mutations.js
@@ -10,3 +10,21 @@ export const LOGIN = gql`
     }
   }
   `;
+
+  export const ADD_ORDER = gql`
+    mutation addOrder($products: [ID]!) {
+      addOrder(products: $products) {
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          category {
+            name
+          }
+        }
+      }
+    }
+  `;
