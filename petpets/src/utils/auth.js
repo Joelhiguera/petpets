692f726f1cfab86cfb91fa00dfd241a14acefd22
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-22 18:53:34 -0600

    create AuthService class and getProfile, loggenIn, isTokenExpired functions

---
+++ petpets/src/utils/auth.js
@@ -0,0 +1,25 @@
+import decode from 'jwt-decode';
+
+class AuthService {
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
+  }
+
+  isTokenExpired(token) {
+    try {
+      const decoded = decode(token); 
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  
+}
