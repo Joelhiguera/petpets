c81e6eb2ffbcfbf64f8886f75311a538131be9e8
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-22 18:46:24 -0600

    create auth.js

--- petpets/src/components/Nav/index.js
+++ petpets/src/components/Nav/index.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <ul className="flex-row">
-      <li className="mx-1">
-        <Link to="/signup">
-          Signup
-        </Link>
-      </li>
-      <li className="mx-1">
-        <Link to="/login">
-          Login
-        </Link>
-      </li>
-    </ul>
-  );
-}
-
-export default Nav;
+        <ul className="flex-row">
+          <li className="mx-1">
+            <Link to="/signup">
+              Signup
+            </Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      );
+    }
+  
+    export default Nav
