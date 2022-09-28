9fe16eaa7bc3dd8e59b25b801c1aeb7131fecc9b
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-28 12:13:56 -0600

    [REF] change category model name to breed and coresponding names

--- server/models/Breed.js
+++ server/models/Breed.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const categorySchema = new Schema({
+const breedSchema = new Schema({
   name: {
     type: String,
     required: true,
@@ -10,6 +10,6 @@ const categorySchema = new Schema({
   }
 });
 
-const Category = mongoose.model('Category', categorySchema);
+const Breed = mongoose.model('Breed', breedSchema);
 
-module.exports = Category;
+module.exports = Breed;
