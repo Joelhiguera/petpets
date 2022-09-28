d7ce9444f6968a3905b14e98fe540fb98e56e76d
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-27 20:05:20 -0600

    [REF] remove items from Nav and resuing Nav for different links

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,31 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function Nav() {
   return (
         <div className="navbarContainer">
-          <h2 className="logo">
-            <Link to="/">
-              PETPETS
-            </Link>
-          </h2>
-        <ul className="navbarUl">
-          <li className="navbarLi">
-            <Link to="/signup">
-              Signup
-            </Link>
-          </li>
-          <li className="navbarLi">
-            <Link to="/login">
-              Login
-            </Link>
-          </li>
-          <li className="navbarLi">
-            <Link to="/Profile">
-              Profile
-            </Link>
-          </li>
-        </ul>
+          <h1>This is the navbar</h1>
         </div>
       );
     }
