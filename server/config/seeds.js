9f2f9661bbf16eb791f066e281556e9b6c8f593d
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-28 12:56:08 -0600

    [IMP] complete seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -42,8 +42,79 @@ db.once('open', async () => {
       gender: 'Male',
       age: 2,
       size: "Medium",
+      color: 'Red/White',
       image: '',
       category: categories[1]._id,
     },
-  ])
-})
+    {
+      name: 'Badger',
+      description:
+        'He is searching for his forever home!',
+      charachteristics: 
+        'Friendly, Affectionate,',
+      gender: 'Male',
+      age: 2,
+      size: "Medium",
+      color: 'Black/White',
+      image: '',
+      category: categories[2]._id,
+    },
+    {
+      name: 'Miss Muffin',
+      description:
+        'She is searching for his forever home!',
+      charachteristics: 
+        'Friendly, Affectionate,',
+      gender: 'Female',
+      age: 5,
+      size: "Medium",
+      color: 'White/black',
+      image: '',
+      category: categories[3]._id,
+    },
+    {
+      name: 'Victoria',
+      description:
+        'She is searching for his forever home!',
+      charachteristics: 
+        'Friendly, Affectionate,',
+      gender: 'Female',
+      age: 2,
+      size: "Small",
+      color: 'Black, Yellow/Tan/Blonde/Fawn',
+      image: '',
+      category: categories[4]._id,
+    },
+    {
+      name: 'Foggy',
+      description:
+        'Foggy was diagnosed with Hydrocephalus. He is very active, but yet has a very mellow side. Due to his condition, he has a few weird quirks, but we would not  change it for the world. He has so much love to give and is quite the character',
+      charachteristics: 
+        'Friendly, Affectionate,',
+      gender: 'Male',
+      age: 2,
+      size: "Small",
+      color: 'Yellow/Tan/Blonde/Fawn',
+      image: '',
+      category: categories[4]._id,
+    },
+    {
+      name: 'Momma',
+      description:
+        'Momma is looking for a forever home!',
+      charachteristics: 
+        'Friendly, Affectionate,',
+      gender: 'Female',
+      age: 4,
+      size: "Medium",
+      color: 'Black, White/Cream',
+      image: '',
+      category: categories[5]._id,
+    },
+    
+  ]);
+
+  console.log('Pets Seeded')
+
+  process.exit();
+});
